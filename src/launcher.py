5f1485e36f3bb29721b90c3f7d77115e4e30f714
Andrey Putilov <Andrey.Putilov@materialplus.io>
2026-01-03 18:37:29 -0800

    refactor: rename .py module files for better redability

--- src/launcher.py
+++ src/launcher.py
@@ -14,27 +14,27 @@ if __name__ == '__main__':
     from lib.mdmreadpy import read_mdd
     from lib.mdmreadpy.lib.mdmreportpy import report_create as report_html_create
     from lib.mdmreport_excel import report_create as report_excel_create
-    from step02_guess_vars import entry as autostk_var_guesser
-    from step03_produce_patch import entry as autostk_patch_generate
-    from step04_text_utility import templater as autostk_templates
+    from program_step02_identify_variables import entry as autostk_var_guesser
+    from program_step03_prepare_patch import entry as autostk_patch_generate
+    from program_step04_write_template import templater as autostk_templates
     from lib.mdmpatchpy import entry as mdd_patch
 elif '.' in __name__:
     # package
     from .lib.mdmreadpy import read_mdd
     from .lib.mdmreadpy.lib.mdmreportpy import report_create as report_html_create
     from .lib.mdmreport_excel import report_create as report_excel_create
-    from .step02_guess_vars import entry as autostk_var_guesser
-    from .step03_produce_patch import entry as autostk_patch_generate
-    from .step04_text_utility import templater as autostk_templates
+    from .program_step02_identify_variables import entry as autostk_var_guesser
+    from .program_step03_prepare_patch import entry as autostk_patch_generate
+    from .program_step04_write_template import templater as autostk_templates
     from .lib.mdmpatchpy import entry as mdd_patch
 else:
     # included with no parent package
     from lib.mdmreadpy import read_mdd
     from lib.mdmreadpy.lib.mdmreportpy import report_create as report_html_create
     from lib.mdmreport_excel import report_create as report_excel_create
-    from step02_guess_vars import entry as autostk_var_guesser
-    from step03_produce_patch import entry as autostk_patch_generate
-    from step04_text_utility import templater as autostk_templates
+    from program_step02_identify_variables import entry as autostk_var_guesser
+    from program_step03_prepare_patch import entry as autostk_patch_generate
+    from program_step04_write_template import templater as autostk_templates
     from lib.mdmpatchpy import entry as mdd_patch
 
 
@@ -51,16 +51,16 @@ def call_report_html_program():
 def call_report_excel_program():
     return report_excel_create.entry_point({'arglist_strict':False})
 
-def call_autostk_var_guesser_program():
+def call_autostk_identify_variables_program():
     return autostk_var_guesser.entry_point({'arglist_strict':False})
 
-def call_autostk_generate_patch_program():
+def call_autostk_prepare_patch_program():
     return autostk_patch_generate.entry_point({'arglist_strict':False})
 
 def call_mdd_patch_program():
     return mdd_patch.entry_point({'arglist_strict':False})
 
-def call_autostk_temp_text_program():
+def call_autostk_write_template_program():
     return autostk_templates.entry_point({'arglist_strict':False})
 
 def call_autostk_test_program():
@@ -79,10 +79,10 @@ run_programs = {
     'report': call_report_html_program,
     'report_html': call_report_html_program,
     'report_excel': call_report_excel_program,
-    'mdd-autostacking-pick-variables': call_autostk_var_guesser_program, # to be renamed to mdd-autostk-identify-variables
-    'mdd-autostacking-prepare-patch': call_autostk_generate_patch_program, # to be renamed to mdd-autostk-prepare-patch
+    'mdd-autostacking-pick-variables': call_autostk_identify_variables_program, # to be renamed to mdd-autostk-identify-variables
+    'mdd-autostacking-prepare-patch': call_autostk_prepare_patch_program, # to be renamed to mdd-autostk-prepare-patch
     'mdd-patch': call_mdd_patch_program,
-    'mdd-autostk-text-utility': call_autostk_temp_text_program, # to be renamed to mdd-autostk-write-templaye
+    'mdd-autostk-text-utility': call_autostk_write_template_program, # to be renamed to mdd-autostk-write-template
     'test': call_autostk_test_program,
 }
 
